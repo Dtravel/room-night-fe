899967232b3276d0713993a1c9d78a5550639fa9
uri-dtravel <uri@dtravel.com>
2023-08-26 09:59:08 +0700

    add time transaction

--- components/property-detail/BookTransaction.tsx
+++ components/property-detail/BookTransaction.tsx
@@ -60,7 +60,7 @@ const BookTransaction: NextPage<Props> = ({ propertyId }) => {
                     <div className='flex flex-col gap-2 items-start'>
                       <span className={'uppercase bg-forest-50 rounded-[16px] px-[10px] py-[2px] text-12-16 font-maison-neue-demi text-forest-700'}>transfer</span>
                       {/* <span className='text-16-20 font-inter-400 text-black'>{el?.value} BNB</span> */}
-                      <span className='text-14-18 font-inter-400 text-black'>{moment(el?.timeStamp, 'X').format("DD/MM/YYYY")}</span>
+                      <span className='text-14-18 font-inter-400 text-black'>{moment(el?.timeStamp, 'X').format("DD/MM/YYYY HH:MM")}</span>
                     </div>
                   </div>
                 )
